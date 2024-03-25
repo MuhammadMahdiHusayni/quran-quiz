f0d20c3f341eb063a23a414b2378b861c07a27e2
Muhammad Mahdi Husayni bin Azizan <mmhusayni2@gmail.com>
2024-03-26 02:46:30 +0800

    init expo router template and add nativewind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    colors: {
+      // Create a custom color that uses a CSS custom value
+      primary: "rgb(var(--color-primary) / <alpha-value>)",
+    },
+  },
+  plugins: [
+    // Set a default value on the `:root` element
+    ({ addBase }) => addBase({ ":root": { "--color-primary": "255 0 0" } }),
+  ],
+}
